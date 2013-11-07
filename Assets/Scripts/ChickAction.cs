f0a9e8c9bb077d8a216331ab2280488a3a7d8550
herox25000 <herox25000@gmail.com>
2013-11-07 18:54:43 +0800

    add a actionscript to chick

---
+++ Assets/Scripts/ChickAction.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//小鸡脚本
+public class ChickAction : MonoBehaviour
+{
+	//移动速度
+	public float moveSpeed = 10;
+
+	void Start()
+	{
+	}
+
+	void Update()
+	{
+		if(Input.GetKey(KeyCode.W))	
+		{
+			transform.Translate(Vector3.up * moveSpeed * Time.deltaTime);
+		}
+		else if(Input.GetKey(KeyCode.S))
+		{
+			transform.Translate(Vector3.down * moveSpeed * Time.deltaTime);
+		}
+
+		if(Input.GetKey(KeyCode.D))	
+		{
+			transform.localScale = new Vector3(1,1,1);
+			transform.Translate(Vector3.right * moveSpeed * Time.deltaTime);
+		}
+		else if(Input.GetKey(KeyCode.A))
+		{
+			transform.localScale = new Vector3(-1,1,1);
+			transform.Translate(Vector3.left * moveSpeed * Time.deltaTime);
+		}
+	}
+}
